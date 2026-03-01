0a6b2367cd0f6f9a9133039436f8119acceec509
JahziaCCC <s300s700@gmail.com>
2026-03-02 02:39:38 +0300

    Create telegram_utils.py

---
+++ telegram_utils.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def send_telegram(bot_token: str, chat_id: str, text: str) -> None:
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    r = requests.post(
+        url,
+        json={
+            "chat_id": chat_id,
+            "text": text,
+            "parse_mode": "HTML",
+            "disable_web_page_preview": True,
+        },
+        timeout=30,
+    )
+    r.raise_for_status()
